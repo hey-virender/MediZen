b5812788979b62c16567d0c20927b04f2e555f5a
Virender Chauhan <bikuchauhan786+github@gmail.com>
2024-05-23 16:22:25 +0530

    minor bug fixed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card1 from "./Card1";
+import Card2 from "./Card2";
 
 const Cards = () => {
   return (
     <div className="flex gap-3 h-80  ">
       <Card1 />
+      <Card2 />
     </div>
   );
 };
